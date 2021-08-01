47d9f247c980ded062d6d9e0b83f4f8b085826f0
Tugba Serik <seriktuba084@gmail.com>
2021-08-01 16:06:40 -0400

    first initial commit

--- src/paxkage1/ClassOne.java
+++ src/paxkage1/ClassOne.java
@@ -5,6 +5,9 @@ public class ClassOne {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("Batch 35/36 learn GitHub");
+        System.out.println("We like Git but we will like more when we have more practice");
+
 
 
 
