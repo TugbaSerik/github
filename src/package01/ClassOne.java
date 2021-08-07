9e4ed6288fd8d06f4457ebe1c65e850454ec4b73
Tugba Serik <seriktuba084@gmail.com>
2021-08-07 17:47:15 -0400

    Third commit

--- src/package01/ClassOne.java
+++ src/package01/ClassOne.java
@@ -6,6 +6,7 @@ public class ClassOne {
 
         System.out.println("Hello world");
         System.out.println("English batch 35/36 Learn github");
+        System.out.println("We Like git but we will like it when we will do more practice");
 
 
 
