99e7d55caacabcdd3ba3cb471af3d1432da1d1d4
Tugba Serik <seriktuba084@gmail.com>
2021-08-07 17:39:45 -0400

    First commit

--- src/package01/ClassOne.java
+++ src/package01/ClassOne.java
@@ -4,11 +4,9 @@ public class ClassOne {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        System.out.println("Batch 35/36 learn github");
-        System.out.println("We like git but we will like it when we will do more practice");
+        System.out.println("Hello world");
 
-        System.out.println("We love git");
+
 
     }
 }
