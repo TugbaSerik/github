23df16fde10d25c6b7ec1f0dcc7021b0ada13489
Tugba Serik <seriktuba084@gmail.com>
2021-08-07 17:45:47 -0400

    Second commit

--- src/package01/ClassOne.java
+++ src/package01/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("English batch 35/36 Learn github");
 
 
 
